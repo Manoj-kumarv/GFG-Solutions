79063181e13f4dcb9b7dcee237e4454ed6e20f91
Manoj Kumar V <manojkumarv724@gmail.com>
2026-06-28 00:06:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/k Times Appearing Adjacent Two 1's/k-times-appearing-adjacent-two-1s.java
@@ -0,0 +1,23 @@
+class Solution {
+    static final int MOD = 1000000007;
+
+    public int countStrings(int n, int k) {
+        int[][][] dp = new int[n + 1][k + 1][2];
+
+        dp[1][0][0] = 1;
+        dp[1][0][1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1]) % MOD;
+
+                dp[i][j][1] = dp[i - 1][j][0]; // 0 → 1
+                if (j > 0) {
+                    dp[i][j][1] = (dp[i][j][1] + dp[i - 1][j - 1][1]) % MOD;
+                }
+            }
+        }
+
+        return (dp[n][k][0] + dp[n][k][1]) % MOD;
+    }
+}
